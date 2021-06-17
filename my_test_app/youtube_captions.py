16a5fe0cc20efdc2b1f79c624d1c5af2ae26c915
Dez113 <dlyakontakt@gmail.com>
2021-06-17 17:20:32 +0300

    get captions in xml(not parsed yet)

--- my_test_app/youtube_captions.py
+++ my_test_app/youtube_captions.py
@@ -2,22 +2,22 @@ import requests
 import json
 from pprint import pprint
 
-video_id = 'SW14tOda_kI'
+VIDEO_ID = 'SW14tOda_kI'
 
 WATCH_URL = 'https://www.youtube.com/watch?v={video_id}'
 
-http_client = requests.Session()
+HTTP_CLIENT = requests.Session()
 
 
 def get_html():
-    return http_client.get(WATCH_URL.format(video_id=video_id)).text.replace(
+    return HTTP_CLIENT.get(WATCH_URL.format(video_id=VIDEO_ID)).text.replace(
         '\\u0026', '&'
     ).replace(
         '\\', ''
     )
 
 
-def _extract_captions_json(html, video_id):
+def extract_captions_json(html, video_id):
     splitted_html = html.split('"captions":')
 
     if len(splitted_html) <= 1:
@@ -34,5 +34,21 @@ def _extract_captions_json(html, video_id):
     return captions_json
 
 
+def get_caption_track_url(captions_json):
+    for track in captions_object['captionTracks']:
+        if 'kind' not in track.keys() and track['baseUrl']:
+            return track['baseUrl']
+        elif 'kind' in track.keys() and track['baseUrl']:
+            return track['baseUrl']
+        else:
+            return ''
+
+
 if __name__ == "__main__":
-    pprint(_extract_captions_json(get_html(), video_id))
+    captions_object = extract_captions_json(get_html(), VIDEO_ID)
+    pprint(HTTP_CLIENT.get(get_caption_track_url(captions_object)).text)
+
+    # pprint(captions_object)
+    # pprint(captions_object['captionTracks'])
+    # for track in captions_object['captionTracks']:
+    #     print(track['baseUrl']) if 'kind' not in track.keys() else print('Track ')
